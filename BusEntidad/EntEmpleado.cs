7be66310d3f8453938e44dbe69e6a385449b8178
leoabmen <ylia12@hotmail.com>
2016-03-30 19:50:22 -0600

    entidades llenas
    ok!

--- BusEntidad/EntEmpleado.cs
+++ BusEntidad/EntEmpleado.cs
@@ -8,12 +8,57 @@ namespace GaboLeo.Empleados.Business.Entidad
 {
     public class EntEmpleado
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        private EntPuesto puesto;
+
+        public EntPuesto Puesto 
+        {
+            get 
+            {
+                if (puesto == null)
+                    puesto = new EntPuesto();
+                return puesto; 
+            }
+            set 
+            { if (puesto==null)
+                puesto = new EntPuesto();
+                puesto = value; 
+            }
+        }
+
+        private EntDepartamento departamento;
+        public EntDepartamento Departamento
+        {
+            get
+            {
+                if (departamento == null)
+                    departamento = new EntDepartamento();
+                return departamento;
+            }
+            set
+            {
+                if (departamento == null)
+                    departamento = new EntDepartamento();
+                departamento = value;
+            }
+        }
+        
+
+        
+
     }
     public class EntPuesto
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        
     }
     public class EntDepartamento
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
 
