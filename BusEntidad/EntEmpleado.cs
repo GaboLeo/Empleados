7e6b29ba745587034c663766186b30e16e07fadb
leoabmen <ylia12@hotmail.com>
2016-03-30 19:24:18 -0600

    Clases
    Leo

---
+++ BusEntidad/EntEmpleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaboLeo.Empleados.Business.Entidad
+{
+    public class EntEmpleado
+    {
+    }
+    public class EntPuesto
+    {
+    }
+    public class EntDepartamento
+    {
+    }
+}
+
